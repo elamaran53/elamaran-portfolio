44b4ea8044ebc891bc735b53e84862c3e890a318
ELAMARAN T <123816585+elamaran53@users.noreply.github.com>
2025-08-05 23:44:38 -0700

    Update Home.jsx
    resume_update

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -27,9 +27,9 @@ export const Home = () => {
             </a>
 
             <a
-              href="./Elamaran Resume.pdf"
+              href="/ELAMARAN_RESUME.pdf"
               className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
-             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10" download={`Elamaran Resume.pdf`}
+             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10" download={`ELAMARAN_RESUME.pdf`}
             >
               Download Cv
             </a>
